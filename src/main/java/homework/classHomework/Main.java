ee0d0fd6e2be19bb96925b4b7917d3d4016e383f
edgars-porietis <104780315+edgars-porietis@users.noreply.github.com>
2022-07-28 23:07:10 +0300

    trijsturis

--- src/main/java/homework/classHomework/Main.java
+++ src/main/java/homework/classHomework/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Trijsturis trijsturis1 = new Trijsturis();
-        
+
 
         System.out.println(trijsturis1.laukums());
 
